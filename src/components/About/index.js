0cec184681d238660417c1420bbb777999e94883
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-09-30 12:52:59 -0400

    Updated About Me section
    added photo and short description
    
    need to make it look better late lol

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,8 +1,16 @@
+import me from '../../assets/images/me.jpg'
+
 function AboutMe() {
   return (
     <section className='mx-5'>
       <h1 id="about">About Me</h1>
-    </section>
+      <div id='me-img'>
+        <img src={me} alt="me" width='250' height='350'/>
+      </div>
+      <div id='me-text'>
+        Back-end web developer with skills that include problem solving, leadership, teamwork, and quick learning. Earned a Full-stack Web Development certificate from Vanderbilt University. 
+      </div>
+    </section>  
   )
 }
 
